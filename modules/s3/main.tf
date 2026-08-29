67330ef679621f850422199e7dbe551d137a7365
Osakpolor Ogieriakhi <osakpolorogieriakhi@gmail.com>
2026-08-29 19:06:07 +0100

    Added file

---
+++ modules/s3/main.tf
@@ -0,0 +1,65 @@
+resource "random_string" "bucket_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "${local.resource_name}-${var.bucket_name}-${random_string.bucket_suffix.result}"
+
+  tags = {
+    Name = "${local.resource_name}-s3_bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
+
+  depends_on = [aws_s3_bucket_versioning.s3_versioning]
+
+  bucket = aws_s3_bucket.s3_bucket.bucket
+
+  rule {
+    id = "config" 
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
---
+++ modules/s3/variable.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+    description = "The name of the project"
+    type = string
+    default = "COB"
+}
+
+variable "environment" {
+    description = "The environment of the project e.g dev, test, staging, prod"
+    type = string
+
+    validation {
+    condition     = contains(["dev", "test", "staging", "prod"], lower(var.environment))
+    error_message = "environment must be one of: dev, test, staging, prod (case-insensitive)"
+  }
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
