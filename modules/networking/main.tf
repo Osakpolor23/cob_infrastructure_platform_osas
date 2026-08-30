4c44b38fb6b9bc2d13ffa00fa92d6cabe17b31f4
Osakpolor Ogieriakhi <osakpolorogieriakhi@gmail.com>
2026-08-30 17:06:15 +0100

    Modified

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -195,7 +195,29 @@ resource "aws_security_group" "security_group" {
   }
 }
 
+resource "aws_security_group" "public_ec2_sg" {
+  name        = "${local.resource_name}-public-ec2-sg"
+  description = "Security group for public-facing EC2 tier"
+  vpc_id      = aws_vpc.vpc.id
 
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${local.resource_name}-public-ec2-sg" }
+}
 
 
 
--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -68,3 +68,6 @@ output "database_route_table_id" {
   value       = aws_route_table.database_route_table.id
 }
 
+output "public_security_group_id" {
+  value = aws_security_group.public_ec2_sg.id
+}
